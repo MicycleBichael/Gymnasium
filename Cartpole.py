1c85502bf768468440fdff3fba6d6b0ff14e1783
MicycleBichael <pototopopato@gmail.com>
2023-01-20 14:15:20 -0500

    experinece replay!!

--- Cartpole.py
+++ Cartpole.py
@@ -7,13 +7,14 @@ from gym.utils.play import play
 env = gym.make("CartPole-v1")
 env2 = gym.make("CartPole-v1",render_mode="human")
 inputs = tf.keras.Input(shape=(4,))
-x = tf.keras.layers.Dense(64, activation=tf.nn.relu)(inputs)
+x = tf.keras.layers.Dense(32, activation=tf.nn.relu)(inputs)
 outputs = tf.keras.layers.Dense(2, activation=tf.nn.relu)(x)
 model = tf.keras.Model(inputs=inputs, outputs=outputs)
-epsilon = 0.5 # Chance of taking a random action
+epsilon = 0.3 # Chance of taking a random action
 discount = 0.12
-learningRate = 0.8
-batchSize = 10 # Number of episodes to train on at once 
+learningRate = 0.3
+batchSize = 1000 # Number of episodes to train on at once 
+batchExperiences = []
 epCount = 1000 # Number of episodes
 decayMult = (0.001/epsilon)**(1/epCount)
 disMult = (0.395/discount)**(2/epCount)
@@ -23,54 +24,45 @@ mse_loss = tf.keras.losses.MeanSquaredError()
 
 def getNextAction(state):
     if np.random.random() > epsilon:
+        state = tf.convert_to_tensor(state.reshape((1,4)))
         return tf.argmax(model(state),axis=-1).numpy()[0]
     else:
         return np.random.randint(2)
-def customLoss(state,state2,reward,action):
-    oldQ = model(state)[0][action]
-    newQ = reward + discount*(tf.reduce_max(model(state2),axis=-1))
-    mse = mse_loss(tf.reshape(newQ,[1]),tf.reshape(oldQ,[1]))
-    return tf.keras.backend.mean(mse)
-'''def updateS(state,state2,reward,action,action2): 
-    oldSARSA = q_values[state,action]
-    return oldSARSA + learningRate*(reward + discount*(q_values[state2,action2]) - oldSARSA)'''
 
 rewardArr = [] # Array of rewards
 lossArr = [] # Array of losses
-epArr = [] # Array of episode numbers
 for _ in range(epCount):
     terminated = False
     truncated = False
     observation, info = env.reset() # Initializes/resets environment, initializes observation and info values with base values
     rewSum = 0
     lossSum = 0
-    observation = tf.convert_to_tensor(observation.reshape((1,4)))
     while terminated == False and truncated == False:
-        state = observation
-        with tf.GradientTape() as tape:
-            if _ < 900:
-                actionIndex = getNextAction(observation)
-            else:
-                actionIndex = tf.argmax(model(state),axis=-1).numpy()[0]
-            observation, reward, terminated, truncated, info = env.step(actionIndex)
-            observation = tf.convert_to_tensor(observation.reshape((1,4)))
-            loss = customLoss(state,observation,reward,actionIndex)
-            gradients = tape.gradient(loss, model.trainable_variables)
-        optimizer.apply_gradients(zip(gradients, model.trainable_variables))
-        rewSum += reward
-        lossSum += loss.numpy()
+        state = observation # Stores previous observation
+        actionIndex = getNextAction(observation) # Uses epsilon-greedy method to choose next action
+        observation, reward, terminated, truncated, info = env.step(actionIndex)
+        batchExperiences.append([state,observation,reward,actionIndex]) # Stores results in a batch array
+        if len(batchExperiences) == batchSize:
+            print(f"\rEpisode: {_+1} || Training, please wait...",end="")
+            inputs = np.array([exp[0] for exp in batchExperiences])
+            targets = []
+            for exp in batchExperiences:
+                temp = tf.convert_to_tensor(exp[1].reshape(1,4))
+                targets.append(exp[2] + discount*np.max(model(temp)))
+            targets = np.array(targets).reshape(batchSize, 1)
+            with tf.GradientTape() as tape:
+                outputs = model(inputs)
+                loss = mse_loss(outputs,targets)
+                gradients = tape.gradient(loss, model.trainable_variables)
+                optimizer.apply_gradients(zip(gradients, model.trainable_variables))
+            batchExperiences = []
+            lossSum += loss.numpy()
+        rewSum += reward    
     lossArr.append(lossSum)
     rewardArr.append(rewSum)
-    epArr.append(_+1)
     #if _ <= epCount/2:
         #discount*=disMult
     print(f"\rEpisode: {_+1}",end="")
-    '''if (_+1) % checkWait == 0:
-        print(f"Score: {rewardAvg/checkWait}\nLoss: {lossAvg/checkWait}\nEpisode: {_+1}")
-        print( "=========================")
-        rewardAvg = 0
-        lossAvg = 0'''
-
 plt.subplot(121)
 plt.plot(rewardArr,'r-',label='score',linewidth=1)
 plt.xlabel('Episode')
