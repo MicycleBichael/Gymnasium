453106b87d5b5ccd713cf841095efb9f9f52fac0
MicycleBichael <pototopopato@gmail.com>
2023-01-22 17:24:05 -0500

    improved readability, implemented early stopping

--- Cartpole.py
+++ Cartpole.py
@@ -1,82 +1,116 @@
+'''
+Trains an algorithm on the Cart Pole environment using deep Q-Learning
+'''
 import gymnasium as gym
 import numpy as np
 import tensorflow as tf
 import matplotlib.pyplot as plt
-from gym.utils.play import play
 
+# Initializes environments
 env = gym.make("CartPole-v1")
-env2 = gym.make("CartPole-v1",render_mode="human")
+env2 = gym.make("CartPole-v1", render_mode="human")
+
+# Initializes model
 inputs = tf.keras.Input(shape=(4,))
 x = tf.keras.layers.Dense(32, activation=tf.nn.relu)(inputs)
 outputs = tf.keras.layers.Dense(2, activation=tf.nn.relu)(x)
 model = tf.keras.Model(inputs=inputs, outputs=outputs)
-epsilon = 0.3 # Chance of taking a random action
-discount = 0.12
-learningRate = 0.3
-batchSize = 1000 # Number of episodes to train on at once 
-batchExperiences = []
-epCount = 1000 # Number of episodes
-decayMult = (0.001/epsilon)**(1/epCount)
-disMult = (0.395/discount)**(2/epCount)
-mode = 'S' # Switch between Q-Learning and SARSA-Learning
-optimizer = tf.optimizers.SGD(learning_rate=learningRate)
-mse_loss = tf.keras.losses.MeanSquaredError()
 
-def getNextAction(state):
-    if np.random.random() > epsilon:
-        state = tf.convert_to_tensor(state.reshape((1,4)))
-        return tf.argmax(model(state),axis=-1).numpy()[0]
+EPSILON = 0.3  # Chance of taking a random action
+DISCOUNT = 0.12
+LEARNING_RATE = 0.3
+BATCH_SIZE = 1050  # Number of episodes to train on at once
+EP_COUNT = 6000  # Number of episodes
+DECAY_MULT = (0.001/EPSILON)**(1/EP_COUNT)  # Decreases EPSILON to 0.001 over EP_COUNT episodes
+MAX_ITERATIONS = 20  # maximum batch iterations before quitting
+batch_experiences = []
+batch_validation = []  # validation set
+optimizer = tf.optimizers.SGD(learning_rate=LEARNING_RATE)
+mse_loss = tf.keras.losses.MeanSquaredError()
+best_model = None
+
+
+def get_next_action(state_in):
+    '''
+    Obtains next action through epsilon greedy algorithm
+    '''
+    if np.random.random() > EPSILON:
+        state_in = tf.convert_to_tensor(state_in.reshape((1, 4)))
+        return tf.argmax(model(state_in), axis=-1).numpy()[0]
     else:
         return np.random.randint(2)
 
-rewardArr = [] # Array of rewards
-lossArr = [] # Array of losses
-for _ in range(epCount):
+
+reward_arr = []  # Array of rewards
+loss_arr = []  # Array of losses
+last_avg = 0  # last average of training sesh
+iterations = 0  # batch iterations without improvement
+episodes = 0  # episodes over last training seshs
+for _ in range(EP_COUNT):
     terminated = False
     truncated = False
-    observation, info = env.reset() # Initializes/resets environment, initializes observation and info values with base values
-    rewSum = 0
-    lossSum = 0
-    while terminated == False and truncated == False:
-        state = observation # Stores previous observation
-        actionIndex = getNextAction(observation) # Uses epsilon-greedy method to choose next action
+    observation, info = env.reset()
+    reward_sum = 0
+    loss_sum = 0
+    while not terminated and not truncated:
+        state = observation  # Stores previous observation
+        if _ % 2 == 0:
+            actionIndex = get_next_action(observation)
+        else:
+            temp_state = tf.convert_to_tensor(observation.reshape((1, 4)))
+            actionIndex = tf.argmax(model(temp_state), axis=-1).numpy()[0]
         observation, reward, terminated, truncated, info = env.step(actionIndex)
-        batchExperiences.append([state,observation,reward,actionIndex]) # Stores results in a batch array
-        if len(batchExperiences) == batchSize:
-            print(f"\rEpisode: {_+1} || Training, please wait...",end="")
-            inputs = np.array([exp[0] for exp in batchExperiences])
+        experience = [state, observation, reward, actionIndex]
+        if _ % 2 == 0:
+            batch_experiences.append(experience)  # Stores results in a batch array
+        else:
+            batch_validation.append(experience)
+        if len(batch_experiences) == BATCH_SIZE:
+            mean = sum([exp[2] for exp in batch_validation])/episodes
+            if mean > last_avg:
+                last_avg = mean
+                best_model = model
+            else:
+                iterations += 1
+            episodes = 0
+            print(f"\rEpisode: {_+1} || Training, please wait...", end="")
+            inputs = np.array([exp[0] for exp in batch_experiences])
             targets = []
-            for exp in batchExperiences:
-                temp = tf.convert_to_tensor(exp[1].reshape(1,4))
-                targets.append(exp[2] + discount*np.max(model(temp)))
-            targets = np.array(targets).reshape(batchSize, 1)
+            for exp in batch_experiences:
+                temp = tf.convert_to_tensor(exp[1].reshape(1, 4))
+                targets.append(exp[2] + DISCOUNT*np.max(model(temp)))
+            targets = np.array(targets).reshape(BATCH_SIZE, 1)
             with tf.GradientTape() as tape:
                 outputs = model(inputs)
-                loss = mse_loss(outputs,targets)
+                loss = mse_loss(outputs, targets)
                 gradients = tape.gradient(loss, model.trainable_variables)
                 optimizer.apply_gradients(zip(gradients, model.trainable_variables))
-            batchExperiences = []
-            lossSum += loss.numpy()
-        rewSum += reward    
-    lossArr.append(lossSum)
-    rewardArr.append(rewSum)
-    #if _ <= epCount/2:
-        #discount*=disMult
-    print(f"\rEpisode: {_+1}",end="")
+            batch_experiences = []
+            batch_validation = []
+            loss_sum += loss.numpy()
+        reward_sum += reward
+    episodes += 1
+    if iterations >= MAX_ITERATIONS:
+        break
+    loss_arr.append(loss_sum)
+    reward_arr.append(reward_sum)
+    print(f"\rEpisode: {_+1}", end="")
+print(f"Best Average Score: {last_avg}")
 plt.subplot(121)
-plt.plot(rewardArr,'r-',label='score',linewidth=1)
+plt.plot(reward_arr, 'r-', label='score', linewidth=1)
 plt.xlabel('Episode')
 plt.legend()
 plt.subplot(122)
-plt.plot(lossArr,'b-',label='loss',linewidth=1)
+plt.plot(loss_arr, 'b-', label='loss', linewidth=1)
 plt.xlabel('Episode')
 plt.legend()
 plt.show()
-#visualization 
+
+# Visualization
 terminated = False
 observation, info = env2.reset()
-observation = tf.convert_to_tensor(observation.reshape((1,4)))
-while(terminated == False):
-    actionIndex = tf.argmax(model(observation),axis=-1).numpy()[0]
+observation = tf.convert_to_tensor(observation.reshape((1, 4)))
+while not terminated:
+    actionIndex = tf.argmax(best_model(observation), axis=-1).numpy()[0]
     observation, reward, terminated, truncated, info = env2.step(actionIndex)
-    observation = tf.convert_to_tensor(observation.reshape((1,4)))
+    observation = tf.convert_to_tensor(observation.reshape((1, 4)))
